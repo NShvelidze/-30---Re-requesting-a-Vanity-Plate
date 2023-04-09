24500f3c9776875af3ec80ef207cd1b5e6a040bf
Nika Shvelidze <117501895+NShvelidze@users.noreply.github.com>
2023-04-09 16:17:00 +0400

    Create test_plates.py

---
+++ test_plates.py
@@ -0,0 +1,18 @@
+from plates import is_valid
+
+def main():
+    test_plates()
+
+def test_plates():
+    assert is_valid("HELLO") == True
+    assert is_valid("HELLO, WORLD") == False
+    assert is_valid("GOODBYE") == False
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("50") == False
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+
+if __name__ == "__main__":
+    main()
+
